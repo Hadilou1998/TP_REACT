de74bcd5ea17783badbd9dd4ed13df5070bfd87b
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-05 14:55:47 +0200

    Updated App.css, App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
 import React from'react';
 import './App.css';
 
-const displayName = event => alert(event.target.id);
-const emojis = [
-  {
-    emoji: '😀',
-    name: 'grinning face'
-  },
-  {
-    emoji: '🎉',
-    name: 'party poppers'
-  },
-  {
-    emoji: '💃',
-    name: 'woman dancing'
+class App extends React.Component {
+  render() {
+    return <div>Nouvelle Application</div>
   }
-];
-
-function App() {
-  const greeting = "greeting";
-  const displayAction = false;
-  return (
-    <div className="container">
-      <h1 id={greeting}>Hello World</h1>
-      {displayAction && <p>I am writing JSX</p>}
-      <ul>
-        {
-          emojis.map(emoji => (
-            <li key={emoji.name} onClick={displayName}>
-              {emoji.emoji} {emoji.name}
-            </li>
-          ))
-        }
-      </ul>
-    </div>
-  );
-}
+};
 
 export default App;
