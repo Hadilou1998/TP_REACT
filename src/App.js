8cc636ce634488045848984cd1906add2ee592a2
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-05 15:21:32 +0200

    Updated Project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from'react';
 import './App.css';
+import Home from './components/Home';
 
 class App extends React.Component {
   render() {
-    return <div>Nouvelle Application</div>
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
   }
 };
 
