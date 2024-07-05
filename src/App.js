008006b295dbd164afdd08837665160fc65413f5
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-05 12:56:42 +0200

    Updated index.html and src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import Home from './components/Home';
+import React from'react';
 import './App.css';
 
+const displayName = event => alert(event.target.id);
+const emojis = [
+  {
+    emoji: '😀',
+    name: 'grinning face'
+  },
+  {
+    emoji: '🎉',
+    name: 'party poppers'
+  },
+  {
+    emoji: '💃',
+    name: 'woman dancing'
+  }
+];
+
 function App() {
-  return <Home />
+  const greeting = "greeting";
+  const displayAction = false;
+  return (
+    <div className="container">
+      <h1 id={greeting}>Hello World</h1>
+      {displayAction && <p>I am writing JSX</p>}
+      <ul>
+        {
+          emojis.map(emoji => (
+            <li key={emoji.name} onClick={displayName}>
+              {emoji.emoji} {emoji.name}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
 }
 
 export default App;
